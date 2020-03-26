42fdb7a79b0759e20695f4c9d3b19a349651e1f9
shelk <shelk04@mail.ru>
2020-03-26 10:50:30 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -209,6 +209,38 @@ def selection():
         print(request.form['sex'])
         return "Форма отправлена"
 
+@app.route('/choice/<planet_name>')
+def planet_name(planet_name):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css"
+                        href="{url_for('static', filename='css/style.css')}" />
+                        <title>Колонизация</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name}</h1>
+                        <p>Эта планета близка к Земле;</p>
+                        <div class="alert alert-success" role="alert">
+                          На ней много необходимых ресурсов;
+                        </div>
+                        <div class="alert alert-primary" role="alert">
+                          На ней есть вода и атмосфера;
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                          На ней есть небольшое магнитное поле;
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                          Наконец, она просто красива!
+                        </div>
+                      </body>
+                    </html>'''
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
