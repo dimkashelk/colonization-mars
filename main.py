432de96fa4a66c1b7f12d85ede0dc046cc845c85
shelk <shelk04@mail.ru>
2020-03-26 13:52:05 +0300

    Пейзажи Марса

--- main.py
+++ main.py
@@ -310,5 +310,52 @@ def sample_file_upload():
         return "Форма отправлена"
 
 
+@app.route('/carousel')
+def carousel():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css"
+                        href="{url_for('static', filename='css/style.css')}" />
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1 align="center">Пейзажи Марса</h1>
+                        <div align="center" id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                          <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="1" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="0"></li>
+                          </ol>
+                          <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                              <img src="static\img\с1.jpg" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="static\img\с2.jpg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="static\img\с3.jpg" alt="Third slide">
+                            </div>
+                          </div>
+                          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                            <span class="icon-prev" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                            <span class="icon-next" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </div>
+                      </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
