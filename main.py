8275b4d2704f2d59c3114ed9b853da0ea32f9470
shelk <shelk04@mail.ru>
2020-03-26 10:57:06 +0300

    Результат отбора

--- main.py
+++ main.py
@@ -209,6 +209,7 @@ def selection():
         print(request.form['sex'])
         return "Форма отправлена"
 
+
 @app.route('/choice/<planet_name>')
 def planet_name(planet_name):
     return f'''<!doctype html>
@@ -222,7 +223,7 @@ def planet_name(planet_name):
                         crossorigin="anonymous">
                         <link rel="stylesheet" type="text/css"
                         href="{url_for('static', filename='css/style.css')}" />
-                        <title>Колонизация</title>
+                        <title>Варианты выбора</title>
                       </head>
                       <body>
                         <h1>Мое предложение: {planet_name}</h1>
@@ -241,6 +242,36 @@ def planet_name(planet_name):
                         </div>
                       </body>
                     </html>'''
+
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css"
+                            href="{url_for('static', filename='css/style.css')}" />
+                            <title>Результаты</title>
+                          </head>
+                          <body>
+                            <h1>Результаты отбора</h1>
+                            <p>Претендента на участие в миссии {nickname}</p>
+                            <div class="alert alert-success" role="alert">
+                              Поздравляем! Ваш рейтинг после {level} этапа отбора
+                            </div>
+                            <p>составляет {rating}</p>
+                            <div class="alert alert-warning" role="alert">
+                              Желаем удачи!
+                            </div>
+                          </body>
+                        </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
