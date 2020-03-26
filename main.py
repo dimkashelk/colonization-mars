54d5602d99c80823c74a9e3d4c39eb3ca3ca06f5
shelk <shelk04@mail.ru>
2020-03-26 09:23:32 +0300

    Реклама с картинкой

--- main.py
+++ main.py
@@ -27,7 +27,45 @@ def promotion():
 def image_mars():
     return '''<h1>Жди нас, Марс!</h1>
     <img src="{}" alt="здесь должна была быть картинка, но не нашлась">
-        <p>Вот она какая, красная планета.</p>'''.format(url_for('static', filename='mars.jpg'))
+        <p>Вот она какая, красная планета.</p>'''.format(url_for('static', filename='img/mars.jpg'))
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css"
+                    href="{url_for('static', filename='css/style.css')}" />
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img width="200" height="200" src="{url_for('static', filename='img/mars.jpg')}"
+                    alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-primary" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      Мы сделаем обитаемыми безжиненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
 
 
 if __name__ == '__main__':
